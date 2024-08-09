cd6cfde270e90eebb4ebb3c30293af17b46753c4
fishingelephants <fishingelephants@gmail.com>
2024-08-09 19:39:26 +0500

    added constants to the baselayout

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "AstroNote Boilerplate";
+export const SITE_DESCRIPTION = "Astro website powered by NotionCMS";
+export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
