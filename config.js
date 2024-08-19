817a1aa88516219ec02236c3e2db30783807bad4
fishingelephants <fishingelephants@gmail.com>
2024-08-19 17:35:14 +0500

    removed the old blog related files

---
+++ config.js
@@ -0,0 +1,3 @@
+export const SITE_TITLE = "AstroNote - Portfolio Kit";
+export const SITE_DESCRIPTION =
+  "Starterkit to build a portfolio page with Astro and NotionCMS";
--- src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const SITE_TITLE = "AstroNote Boilerplate";
-export const SITE_DESCRIPTION = "Astro website powered by NotionCMS";
-export const HOMEPAGE_ARTICLE_LIMIT = 3;
-export const ARTICLES_PER_PAGE = 6;
