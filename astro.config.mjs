3e424fe0c989e31f617f5c5f0abc595767934de6
fishingelephants <fishingelephants@gmail.com>
2024-08-29 20:58:30 +0500

    added the blog and it is now working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import { remarkReadingTime } from "./src/utils/readingTime";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://astro-frieren-blog-kit.netlify.app/", // Update this to your site URL if needed
+  markdown: {
+    syntaxHighlight: false, // Disable built-in syntax highlighting from Astro
+    remarkPlugins: [remarkReadingTime], // Removed rehypePrettyCode
+  },
+  integrations: [tailwind(), react(), sitemap()],
+  output: "static", // This is fine for a static site
 });
