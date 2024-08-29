b75197aa3b5e6f53d8f8c46dccc2ef2ab8be502c
fishingelephants <fishingelephants@gmail.com>
2024-08-29 19:39:09 +0500

    initial commit

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+export const AppConfig = {
+	site_name: 'Frieren Land',
+	title: 'Frieren Land - Fan Club',
+	description: 'A Place For Fans to Meet',
+	author: 'reifu',
+	locale_region: 'en',
+	locale: 'en'
+};
