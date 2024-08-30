6ed25a812ceaf1c4f6919bacde444492b287825b
fishingelephants <fishingelephants@gmail.com>
2024-08-30 13:36:45 +0500

    refactored codes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,27 @@
+const disabledCss = {
+  "code::before": false,
+  "code::after": false,
+  "blockquote p:first-of-type::before": false,
+  "blockquote p:last-of-type::after": false,
+  pre: false,
+  code: false,
+  "pre code": false,
+};
+
+/** @type {import('tailwindcss').Config} */
 export default {
-  // your tailwind config here
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./node_modules/flowbite/**/*.js",
-  ],
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: { css: disabledCss },
+        sm: { css: disabledCss },
+        lg: { css: disabledCss },
+        xl: { css: disabledCss },
+        "2xl": { css: disabledCss },
+      },
+    },
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require("@tailwindcss/typography")],
 };
