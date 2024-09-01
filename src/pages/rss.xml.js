22ed020281decfc7940d4ceb300a5a5e9b1a839f
fishingelephants <fishingelephants@gmail.com>
2024-09-01 21:32:11 +0500

    Added title and description

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,14 +1,13 @@
-import rss, {pagesGlobToRssItems} from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 
 export async function GET(context) {
   return rss({
-    title: 'nicdun.dev - blog',
-    description: 'Crafting the Digital Future with Web Development Wonders',
+    title: "Welcome to the Frieren Land!",
+    description:
+      "Join me on this exciting adventure as we explore the magical landscape together",
     site: context.site,
-    items: await pagesGlobToRssItems(
-      import.meta.glob('./posts/*.{md,mdx}'),
-    ),
-    stylesheet: './rss/styles.xsl',
+    items: await pagesGlobToRssItems(import.meta.glob("./posts/*.{md,mdx}")),
     customData: `<language>en-us</language>`,
   });
 }
+
